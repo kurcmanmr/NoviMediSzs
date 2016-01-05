acc829b9c53eee43f0d954ee221b61ca3d6dff10
mihailoradosavljevic <mihailoradosavljevic@gmail.com>
2016-01-05 15:17:50 +0100

    evo

--- TESTAAPI/TESTAAPI/App_Start/BundleConfig.cs
+++ TESTAAPI/TESTAAPI/App_Start/BundleConfig.cs
@@ -23,6 +23,11 @@ namespace TESTAAPI
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+
+            bundles.Add(new ScriptBundle("~/bundles/angular").Include(
+
+                       "~/Scripts/angular.js"));
+
         }
     }
 }
