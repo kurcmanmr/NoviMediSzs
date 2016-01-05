837166e8eaecd9eda0a1c4c7a7a34cd2a5e6885a
stijepo303 <stijepo303@gmail.com>
2016-01-05 08:59:04 +0100

    proba

---
+++ jos_jednom/jos_jednom/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(jos_jednom.Startup))]
+namespace jos_jednom
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
